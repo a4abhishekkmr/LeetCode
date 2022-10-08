d56c0aff9f5b2daa30f30d3a28cf626f9f990b25
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-08 11:36:08 +0530

    Time: 35 ms (60.59%), Space: 50.6 MB (46.93%) - LeetHub

---
+++ 1828-queries-on-number-of-points-inside-a-circle/1828-queries-on-number-of-points-inside-a-circle.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] countPoints(int[][] points, int[][] queries) {
+        //we need to find all the points which are inside or at the edge of particular circle so that it will be returned to answer
+        
+        //Basic Maths
+        //Distance of two points is Sq.root(x-x1)^2 +(y-y1)^2 and we need this to be less than or equal to r*r.. since we are given only 500 points so we can consider for brute force apprach of N^2 approach
+        
+        //x and y will be cordinates of centre of circle and x1,y1 will be the points to check..
+            //better than sorting and picking as that might be risky for out of radius
+            //Thus, we need - (circle_center_x - x1) ^ 2 - (circle_center_y - y1) ^ 2 <= r * r for a point to be inside the circle.
+        
+        int count=0;
+        int res[]=new int[queries.length];
+        
+        for(int i=0;i<queries.length;i++)
+        {
+            int x=queries[i][0],y=queries[i][1],r=queries[i][2];
+            for(int j=0;j<points.length;j++)
+            {
+                if((x-points[j][0])*(x-points[j][0])+(y-points[j][1])*(y-points[j][1])<=(r*r))
+                    count++;
+            }
+            res[i]=count;
+            count=0;
+        }
+        return res;
+    }
+}
