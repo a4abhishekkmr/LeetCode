aeca79bb139e52ca6482b7bfd7dbcc4ff30d7385
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-09 00:19:00 +0530

    Time: 4 ms (90.97%), Space: 47.9 MB (29.58%) - LeetHub

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -4,17 +4,45 @@ class Solution {
         //create an returning array of size bixes.length
         
         int n=boxes.length();
-        int ans[]=new int[n];
+         int ans[]=new int[n];
+        
+//         for(int i=0;i<n;i++)
+//         {
+//             int x=0;
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(boxes.charAt(j)=='1')
+//                     x+=Math.abs(i-j);
+//             }
+//             ans[i]=x;
+//         }
+//         return ans;
+        
+        //Another approach
+        //calculating sum approach
+        //taking example of "001011" where for first char i have 3 1's totaling 2+4+5=11
+        //now for char at 2. i have to substract 3 as all 1's need less 1 times so total 3 place and result will be 11-3=8
+        //similarly for next it will be 8-3=5
+        //now at index = 3, i can see there are 2 balls that will take 5 operations for index 2 and there is 1 ball on the left which will need to traverse 1 step ahaed then total steps = 5 - 2 + 1 => 4
+        
+        int count=0,sum=0;
         
         for(int i=0;i<n;i++)
         {
-            int x=0;
-            for(int j=0;j<n;j++)
-            {
-                if(boxes.charAt(j)=='1')
-                    x+=Math.abs(i-j);
-            }
-            ans[i]=x;
+            if(boxes.charAt(i)=='1')
+            {count++;sum+=i;}
+        }
+        ans[0]=sum;
+        
+        int backcount=0;
+        if(boxes.charAt(0)=='1')
+            {count--;backcount++;}
+        
+        for(int i=1;i<n;i++)
+        {
+            ans[i]=ans[i-1]-count+backcount;
+            if(boxes.charAt(i)=='1')
+            {count--;backcount++;}
         }
         return ans;
     }
