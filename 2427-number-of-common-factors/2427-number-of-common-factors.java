1092ad4732c611db9b3451c0ee41cabd85ee067d
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-10 00:00:56 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (38.11%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -1,15 +1,39 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        int count=1;
+    //    int count=1;
         
+//         int min=a<b?a:b;
+        
+//         for(int i=2;i<=min;i++)
+//         {
+//             if(a%i==0 && b%i==0)
+//                 count++;
+//         }
+        
+//         return count;
+        
+        
+        //Another Method is to find gcd of both these nums and then find factors of gcd
         int min=a<b?a:b;
+        int max=a>b?a:b;
         
-        for(int i=2;i<=min;i++)
-        {
-            if(a%i==0 && b%i==0)
-                count++;
-        }
+        int g=gcd(a,b);//small then big
         
+        return factors(g);
+        
+    }
+    private int gcd(int a,int b)
+    {
+        if(b==0) return a;
+        
+        else return gcd(b,a%b);
+        
+    }
+    private int factors(int n)
+    {
+        int count=1;
+        for(int i=2;i<=n;i++)
+            if(n%i==0) count++;
         return count;
     }
 }
