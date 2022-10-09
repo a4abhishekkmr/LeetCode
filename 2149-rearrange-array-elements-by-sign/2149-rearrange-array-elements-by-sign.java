e992b01b8391c0110c0d78b7253848cc49b5fdb2
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-10 00:56:52 +0530

    Time: 7 ms (75.54%), Space: 216.4 MB (61.03%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -3,24 +3,40 @@ class Solution {
         //either to take 2 arrays of pos and neg.
         
         //1st approach
-        int pos[]=new int[nums.length/2];
-        int neg[]=new int[nums.length/2];
+//         int pos[]=new int[nums.length/2];
+//         int neg[]=new int[nums.length/2];
         
-        int j=0,k=0;
+//         int j=0,k=0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i]>=0)
+//                 pos[j++]=nums[i];
+                
+//             else
+//                 neg[k++]=nums[i];
+//         }
+        
+//         for(int i=0;i<nums.length/2;i++)
+//         {
+//             nums[2*i]=pos[i];
+//             nums[2*i+1]=neg[i];
+//         }
+//         return nums;
+        
+        //Either use two pointer approach(different way)
+        
+        int ans[]=new int[nums.length];
+        int j=0,k=1;
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]>=0)
-                pos[j++]=nums[i];
-                
+            {ans[j]=nums[i];j=j+2;}
             else
-                neg[k++]=nums[i];
+            {
+                ans[k]=nums[i];k=k+2;
+            }
         }
+        return ans;
         
-        for(int i=0;i<nums.length/2;i++)
-        {
-            nums[2*i]=pos[i];
-            nums[2*i+1]=neg[i];
-        }
-        return nums;
     }
 }
