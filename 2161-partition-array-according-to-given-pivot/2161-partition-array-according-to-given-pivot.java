028c5b556ca2e53974f798f87abdb3f9bd6de8f1
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-10 23:50:59 +0530

    Time: 7 ms (86.87%), Space: 169.2 MB (19.36%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -6,23 +6,57 @@ class Solution {
         
         int pivot_count=0;
         int small=0;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i]<pivot)
+//                 ans[small++]=nums[i];
+//             else if(nums[i]==pivot)
+//                 pivot_count++;
+//         }
+//         for(int i=0;i<pivot_count;i++)
+//         {
+//             ans[small++]=pivot;
+//         }
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(nums[i]>pivot)
+//                 ans[small++]=nums[i];
+//         }
+        
+//         return ans;
+        
+        //2nd Approach
+        int large=nums.length-1;
+        
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]<pivot)
                 ans[small++]=nums[i];
-            else if(nums[i]==pivot)
+            else if(nums[i]>pivot)
+                ans[large--]=nums[i];
+            else
                 pivot_count++;
         }
         for(int i=0;i<pivot_count;i++)
         {
             ans[small++]=pivot;
         }
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]>pivot)
-                ans[small++]=nums[i];
-        }
         
-        return ans;
+        
+        return reverse(ans,large+1,nums.length-1);
+        
+    }
+    
+    private int[] reverse(int nums[],int start,int end)
+    {
+        while(start<end)
+        {
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            
+            start++;end--;
+        }
+        return nums;
     }
 }
