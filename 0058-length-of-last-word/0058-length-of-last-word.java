fa2827073ed8270a27a6e93dfc0e8888fb91ee75
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-23 02:13:15 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (65.36%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        
+        String str=s.trim();//The trim() method removes whitespace from both ends of a string.
+            
+            int count=0;
+            for(int i=str.length()-1;i>=0;i--)
+            {
+                if(str.charAt(i)==' ')
+                    break;
+                
+                count++;
+            }
+        return count;
+    }
+}
