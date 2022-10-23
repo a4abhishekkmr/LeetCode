04c364aba485560e691b9ffa96e8c6e858e2ac5e
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-24 00:55:37 +0530

    Time: 1 ms (44.40%), Space: 42.7 MB (40.48%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,45 +1,62 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
-        int start[]=new int[matrix.length];
+//         int start[]=new int[matrix.length];
         
-        for(int i=0;i<matrix.length;i++)
-        {
-            start[i]=matrix[i][0];
-        }
+//         for(int i=0;i<matrix.length;i++)
+//         {
+//             start[i]=matrix[i][0];
+//         }
         
-        int row=findrow(start,target);
+//         int row=findrow(start,target);
         
-        //since sorted so will do binary Search
-        System.out.print(row);
-        return bsearch(matrix,row,target);
-    }
+//         //since sorted so will do binary Search
+//         System.out.print(row);
+//         return bsearch(matrix,row,target);
+//     }
     
-    private int findrow(int []arr,int val)
-    {
-        int index=0;
-        for(int i=1;i<arr.length;i++)
-        {
-            if(arr[i]>val)
-                return i-1;
-        }
-        return arr.length-1;
-    }
+//     private int findrow(int []arr,int val)
+//     {
+//         int index=0;
+//         for(int i=1;i<arr.length;i++)
+//         {
+//             if(arr[i]>val)
+//                 return i-1;
+//         }
+//         return arr.length-1;
+//     }
     
-    private boolean bsearch(int[][] matrix,int row,int target)
-    {
-        int lo=0;
-        int hi=matrix[0].length-1;
+//     private boolean bsearch(int[][] matrix,int row,int target)
+//     {
+//         int lo=0;
+//         int hi=matrix[0].length-1;
         
-        while(lo<=hi)
+//         while(lo<=hi)
+//         {
+//             int mid=lo+(hi-lo)/2;
+//             if(matrix[row][mid]==target)
+//                 return true;
+//             else if(matrix[row][mid]>target)
+//                 hi=mid-1;
+//             else
+//                 lo=mid+1;
+//         }
+//         return false;
+//     }
+        
+     //2nd Approach
+        
+        int n=matrix[0].length;
+        for(int i=matrix.length-1;i>=0;i--)
         {
-            int mid=lo+(hi-lo)/2;
-            if(matrix[row][mid]==target)
-                return true;
-            else if(matrix[row][mid]>target)
-                hi=mid-1;
-            else
-                lo=mid+1;
+            if(matrix[i][0]>target)
+                continue;
+            
+            for(int j=0;j<n;j++)
+            {
+                if(matrix[i][j]==target)
+                    return true;
+            }
         }
         return false;
     }
