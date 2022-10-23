7745cc10cdddf345bfe039cec2c15a3fc496c1a5
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-24 01:08:44 +0530

    Time: 9 ms (27.88%), Space: 50 MB (49.89%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,20 +1,64 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        //1st method using HAshmap
+//         HashMap<Integer,Integer> hm=new HashMap<>();
         
-        HashMap<Integer,Integer> hm=new HashMap<>();
+//         int ans[]=new int[2];
+        
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             if(hm.containsKey(target-nums[i]))
+//             {
+//                 ans[0]=hm.get(target-nums[i])+1;
+//                 ans[1]=i+1;
+//                 return ans;
+//             }
+//             hm.put(nums[i],i);
+//         }
+//         return ans;
+        
+        //2ND METHOD
+        //USE OF SORTED ARRAY
         
         int ans[]=new int[2];
-        
         for(int i=0;i<nums.length;i++)
         {
-            if(hm.containsKey(target-nums[i]))
+            int val= bsearch(nums,target-nums[i]);
+            if(val!=-1)
             {
-                ans[0]=hm.get(target-nums[i])+1;
-                ans[1]=i+1;
+                if(val<i)
+                {
+                    ans[0]=val+1;
+                    ans[1]=i+1;
+                    
+                }
+                else if(val==i)
+                {ans[0]=i+1;
+                 ans[1]=i+2;
+                }
+                else
+                {
+                ans[0]=i+1;
+                 ans[1]=val+1;
+                }
                 return ans;
             }
-            hm.put(nums[i],i);
         }
         return ans;
+    }
+    private int bsearch(int []arr,int find)
+    {
+        int lo=0,hi=arr.length-1;
+        while(lo<=hi)
+        {
+            int mid=lo+(hi-lo)/2;
+            if(arr[mid]==find)
+                return mid;
+            else if(arr[mid]>find)
+                hi=mid-1;
+            else
+                lo=mid+1;
+        }
+        return -1;
     }
 }
