24dbcaac063c94b212ea8d316abdad2e993c1cb7
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-26 01:16:01 +0530

    Time: 160 ms (11.14%), Space: 118.3 MB (31.62%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        //find the distance and choose the min k
+        
+        //given 1 <= k <= points.length <= 104
+        
+        int n=points.length;
+        
+        HashMap<Double,List<Integer>> map=new HashMap<>();
+        PriorityQueue<Double> pq=new PriorityQueue<>(Collections.reverseOrder());
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            int []point=points[i];
+            int a=point[0];
+            int b=point[1];
+            double square=helper(a,b);
+
+        List<Integer> al=map.getOrDefault(square,new ArrayList<>());
+            al.add(i);
+            map.put(square,al);
+            
+            if(pq.size()<k){pq.add(square);}
+            else
+            {
+                if(pq.peek()>square)
+                {
+                    pq.poll();
+                    pq.offer(square);
+                }
+            }
+        }
+        int [][]ans=new int[k][2];
+        
+        int i=0;
+        
+        //put time
+        while(pq.size()>0)
+        {
+            double val=pq.poll();
+            if(map.containsKey(val))
+            {
+                List<Integer> al=map.get(val);
+                for(int x:al)
+                {int[] point=points[x];
+                ans[i++]=point;}
+            }
+            map.remove(val);
+        }
+        return ans;
+        
+    }
+    
+    private double helper(int x1,int y1)
+    {
+        //int x1=Math.abs(x-0);
+        //int y1=Math.abs(y-0);
+        
+        x1*=x1;
+        y1*=y1;
+        
+        double ans=Math.sqrt(x1+y1);
+        return ans;
+    }
+}
+
