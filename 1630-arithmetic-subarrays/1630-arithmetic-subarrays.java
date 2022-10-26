71ec6c92512693472fb1eff16462f0ce365cfa76
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-26 21:51:47 +0530

    Time: 2306 ms (5.10%), Space: 93.9 MB (5.10%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -1,43 +1,39 @@
 class Solution {
     public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
-        List<Boolean> al= new ArrayList<>();
-            
-            
-            
-            for(int i=0;i<l.length;i++)
-            {
-                    int n=r[i]-l[i]+1;
-                    int temp[]=new int[n];
-                    int pos=0;
-                    for(int j=l[i];j<=r[i];j++)
-                    {
-                            temp[pos++]=nums[j];
-                           // System.out.println(temp[pos-1]);
-                    }
-                    
-                    boolean ans=checkIsAP(temp,n);
-                    
-                    al.add(ans);
-            }
-            
-            return al;
-    }
-boolean checkIsAP(int arr[], int n)
-    {
-        if (n == 1)
-            return true;
-         
-        // Sort array
-        Arrays.sort(arr);
-         
-        // After sorting, difference between
-        // consecutive elements must be same.
-        int d = arr[1] - arr[0];
-        for (int i = 2; i < n; i++)
-            if (arr[i] - arr[i-1] != d)
-                return false;
-         
-        return true;
+        //Brute force is cleary visible for me
         
+        int n=l.length;
+        
+        List<Boolean> ans=new ArrayList<>(n);
+        
+        for(int i=0;i<n;i++)
+        {
+            int lo=l[i];
+            int hi=r[i];
+            
+            int b[] = Arrays.copyOfRange(nums, lo,hi+1);
+            Arrays.sort(b);
+            
+            //print
+            for(int j=0;j<b.length;j++)
+                System.out.print(b[j]+" ");
+            System.out.println();
+            
+            ans.add(check(b));
+        }
+        return ans;
+    }
+    private boolean check(int []arr)
+    {
+        
+        if(arr.length<=2) return true;
+        int diff=Math.abs(arr[1]-arr[0]);
+        
+        for(int i=2;i<arr.length;i++)
+        {
+            if(Math.abs(arr[i]-arr[i-1])!=diff)
+                return false;
+        }
+        return true;
     }
 }
