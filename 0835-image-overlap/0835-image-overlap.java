64b3847a3c50c2d06589ef6e29dc56c53b5a7b0e
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-27 22:51:43 +0530

    Time: 71 ms (52.12%), Space: 43 MB (72.73%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.java
+++ 0835-image-overlap/0835-image-overlap.java
@@ -1,3 +1,31 @@
+/*
+Intuition:
+If we do brute force, we have 2N horizontal possible sliding, 2N vertical sliding and N^2 to count overlap area.
+We get O(N^4) solution and it may get accepted.
+But we waste out time on case of sparse matrix.
+
+
+Explanation:
+Assume index in A and B is [0, N * N -1].
+
+Loop on A, if value == 1, save a coordinates i / N * 100 + i % N to LA.
+Loop on B, if value == 1, save a coordinates i / N * 100 + i % N to LB.
+Loop on combination (i, j) of LA and LB, increase count[i - j] by 1.
+If we slide to make A[i] orverlap B[j], we can get 1 point.
+Loop on count and return max values.
+I use a 1 key hashmap. Assume ab for row and cd for col, I make it abcd as coordinate.
+For sure, hashmap with 2 keys will be better for understanding.
+
+
+Complexity:
+Assume A the number of points in the image A
+B the number of points in the image B,
+N = A.length = B.length.
+O(N^2) time for preparing,
+and O(AB) time for loop.
+So overall O(AB + N^2) time.
+Space is O(A + B).
+*/
 class Solution {
     public int largestOverlap(int[][] img1, int[][] img2) {
         
