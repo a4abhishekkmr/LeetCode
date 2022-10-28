01c521701177864292d79df270ed1266858cad43
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-10-28 21:53:38 +0530

    Time: 13 ms (75.98%), Space: 55.8 MB (52.25%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        //ok the idea coming to me is I will take the String element and make it to char array then sort it and then check with the help of HahsMap
+        
+        List<List<String>> ans=new ArrayList<>();
+        
+        HashMap<String,ArrayList<String>> hm=new HashMap<>();
+        
+        for(int i=0;i<strs.length;i++)
+        {
+            String curr=strs[i];
+            char []arr=curr.toCharArray();
+            Arrays.sort(arr);
+            
+            String formed=new String(arr);
+            if(hm.containsKey(formed))
+                hm.get(formed).add(curr);
+            
+            else
+            {
+                hm.put(formed,new ArrayList<>());
+                hm.get(formed).add(curr);
+            }
+        }
+        for (List<String> item: hm.values()) {
+    	ans.add(item);
+        }
+        return ans;
+    }
+}
