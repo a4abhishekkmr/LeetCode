cf20cbcc191b0aa382af38286f81bf9665aeef67
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-11-05 23:28:10 +0530

    Time: 2 ms (100.00%), Space: 42.7 MB (88.90%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -4,15 +4,24 @@ class Solution {
         
         for(int i=0;i<arr.length;i++)
         {
-            arr[i]+=Integer.bitCount(arr[i])*10001;
+            arr[i]+=Integer.bitCount(arr[i])*1000001;
         }
         
         Arrays.sort(arr);
         
         for(int i=0;i<arr.length;i++)
         {
-            arr[i]=arr[i]%10001;
+            arr[i]=arr[i]%1000001;
         }
         return arr;
     }
 }
+//Algorithm:
+// For eg: just take a arra like [0,1,2,3,4,5,6,7,8]
+// step 1: After the first for loop...
+// it will look like [0,10001,10003,20004,10005,20006,20007,30008,10009] (don't forget to add the number also)
+// Step 2:sort the array...
+// It, will look like [0,10001,10003,10005,10009,20004,20006,20007,30008]
+// step 3: divide the same as 10001
+// You, will get the result array...
+// Pls,upvote it helps!
