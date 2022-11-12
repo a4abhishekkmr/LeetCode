4cc17d6f6d3b9270c65b298e36156f0f69e60c25
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-11-13 00:00:44 +0530

    Time: 908 ms (5.01%), Space: 119.8 MB (79.22%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -6,16 +6,16 @@ class MedianFinder {
     }
     
     public void addNum(int num) {
-        int i;
-        if(al.size() > 0){
-            for (i = 0; (i < al.size()  && al.get(i) < num); i++);
-            if(i == -1){
-                i = 0;
+        int flag=0;
+        for(int i=0;i<al.size();i++){
+            if(al.get(i)>=num){
+                al.add(i,num);
+                flag=1;
+                break;
             }
-            al.add(i , num);
-        }else{
-            al.add(num);
         }
+        if(flag==0)
+            al.add(num);
     }
     
     public double findMedian() {
