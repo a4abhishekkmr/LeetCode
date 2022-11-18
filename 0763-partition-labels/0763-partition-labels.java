594c168ac03c914be3927af69b193a24f5c67eb5
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-11-18 14:54:59 +0530

    Time: 18 ms (25.97%), Space: 42.4 MB (75.45%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -2,11 +2,39 @@ class Solution {
     public List<Integer> partitionLabels(String s) {
         
         ArrayList<Integer> ans=new ArrayList<>();
-        HashMap<Character,Integer> hm=new HashMap<>();
+//         HashMap<Character,Integer> hm=new HashMap<>();
+        
+//         for(int i=0;i<s.length();i++)
+//         {
+//             hm.put(s.charAt(i),i);
+//         }
+        
+//         int prev=-1;
+//         int max=0;
+        
+//             //formula = max-prev
+            
+//             for(int i=0;i<s.length();i++)
+//             {
+//                     max=Math.max(max,hm.get(s.charAt(i)));
+                    
+//                     //
+//                     if(i==max)
+//                             //partition time
+//                     {
+//                             ans.add(max-prev);
+//                             prev=max;//aage badha die
+//                     }
+                    
+//             }
+//             return ans;
+        
+        int arr[]=new int[26];
         
         for(int i=0;i<s.length();i++)
         {
-            hm.put(s.charAt(i),i);
+            int ch=s.charAt(i)-'a';
+            arr[ch]=i;
         }
         
         int prev=-1;
@@ -16,7 +44,8 @@ class Solution {
             
             for(int i=0;i<s.length();i++)
             {
-                    max=Math.max(max,hm.get(s.charAt(i)));
+                int ch=s.charAt(i)-'a';
+                    max=Math.max(max,arr[ch]);
                     
                     //
                     if(i==max)
