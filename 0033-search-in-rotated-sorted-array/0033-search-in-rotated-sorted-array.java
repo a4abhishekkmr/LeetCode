4b1f1a3e3825f5c6dd529aa4222edac3f3e1b4fb
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-11-23 11:25:13 +0530

    Time: 1 ms (81.30%), Space: 42.8 MB (27.47%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -8,15 +8,18 @@ class Solution {
             int mid=low+(high-low)/2;
             
             if(arr[mid]==x) return mid;
+            //upto here is like normal binary search
+            
             
             if(arr[low]<=arr[mid])
             {
-                if(x>=arr[low] && x<arr[mid])
+                //left half sorted method
+                if(x>=arr[low] && x<arr[mid])//concept of checking first to mid 
                     high=mid-1;
                 else
                     low=mid+1;
             }
-            else
+            else//right half sorted and find
             {
                 if(x>arr[mid] && x<=arr[high])
                     low=mid+1;
