cd775bd6fb4e15dec0aa48dae405bd68c968e47f
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-11-26 22:20:54 +0530

    Time: 3 ms (35.46%), Space: 54.1 MB (8.60%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        //i will make 2 arrays that will store the sum of its left and its right.
+        //then i will iterate on both the arrays if i fetch as same then returns the position else -1;
+        
+        int left[]=new int[nums.length];
+        int right[]=new int[nums.length];
+        
+        for(int i=1;i<nums.length;i++)
+        {
+            left[i]=left[i-1]+nums[i-1]; 
+        }
+        
+        int n=nums.length;
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            right[i]=right[i+1]+nums[i+1];
+        }
+//         for(int i=0;i<nums.length;i++)
+//         {System.out.print(left[i]+" ");
+          
+//         }
+//         System.out.println();
+//         for(int i=0;i<nums.length;i++)
+//         {System.out.print(right[i]+" ");
+          
+//         }
+        for(int i=0;i<nums.length;i++)
+        {
+            if(left[i]==right[i]) return i;
+        }
+        return -1;
+    }
+}
