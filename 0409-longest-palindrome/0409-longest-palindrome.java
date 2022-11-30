2eea197237df7b8c9d160d91d83328613c41cc1d
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-11-30 14:04:04 +0530

    Time: 27 ms (5.62%), Space: 42.9 MB (15.51%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -25,3 +25,46 @@ class Solution {
 
 //Master Solution is here
 //https://leetcode.com/problems/longest-palindrome/discuss/2714192/JAVA-or-3-Approaches-or-Everything-you'd-need
+/*
+
+1. Using HashSet:
+class Solution {
+    public int longestPalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+        int len = 0;
+
+        for (char c : s.toCharArray()) {
+            if (set.contains(c)) {
+                len += 2;
+                set.remove(c);
+            }
+            else set.add(c);
+        }
+
+        return set.size() > 0 ? len + 1 : len;
+    }
+}
+
+// TC: O(n), SC: O(n)
+
+3. Using Frequency array:
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] charCount = new int[128];
+
+        for (char c : s.toCharArray()) {
+            charCount[c]++;
+        }
+
+        int len = 0;
+
+        for (int count : charCount) {
+            len += 2 * (count / 2);
+        }
+
+        return len < s.length() ? len + 1 : len;
+    }
+}
+
+// TC: O(n), SC: O(1)
+*/
