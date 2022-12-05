ddc2e0861bacdb2d4d4aa694b0e55100c8db1b93
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-12-05 18:54:39 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (83.19%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -6,16 +6,26 @@ class Solution {
         
         //2nd method iterative
         
-        int a=0;
-        int b=1;
-        int c=0;
-        for(int i=2;i<=n;i++)
-        {
-            c=a+b;
-            a=b;
-            b=c;
-            //System.out.print(i+" "+c+"   =>");
+        // int a=0;
+        // int b=1;
+        // int c=0;
+        // for(int i=2;i<=n;i++)
+        // {
+        //     c=a+b;
+        //     a=b;
+        //     b=c;
+        //     //System.out.print(i+" "+c+"   =>");
+        // }
+        // return c;
+        
+        
+        // create a dp array to store previous values
+        int dp[]=new int[n+1];
+        dp[1]=1;
+        dp[2]=1;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        return c;
+        return dp[n];
     }
 }
