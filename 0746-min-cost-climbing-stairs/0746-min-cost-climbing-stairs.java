283cc695bf82f1e80ce773e74ea6d98f999c40ac
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-12-07 00:42:53 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (89.83%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -6,19 +6,33 @@ class Solution {
         //ok so here we go. we will take array of size n+2 basically used for tabulation and pick the minimum ones.
         
         int n=cost.length;
-        int []dp=new int[n];
+//         int []dp=new int[n];
         
-        dp[0]=cost[0];
-        dp[1]=cost[1];
+//         dp[0]=cost[0];
+//         dp[1]=cost[1];
         
-        int min=0;
+//         int min=0;
+//         for(int i=2;i<n;i++)
+//         {
+//             if(dp[i-2]<dp[i-1])
+//                 min=dp[i-2];
+//             else min=dp[i-1];
+//             dp[i]=min+cost[i];
+//         }
+//         return Math.min(dp[n-1],dp[n-2]);//as we are stooping at the last or second last.
+        
+        
+        //space optimized
+        
+        int first=cost[0];
+        int second=cost[1];
+        
         for(int i=2;i<n;i++)
         {
-            if(dp[i-2]<dp[i-1])
-                min=dp[i-2];
-            else min=dp[i-1];
-            dp[i]=min+cost[i];
+            int res=Math.min(first,second)+cost[i];
+            first=second;
+            second=res;
         }
-        return Math.min(dp[n-1],dp[n-2]);//as we are stooping at the last or second last.
+        return Math.min(first,second);
     }
 }
