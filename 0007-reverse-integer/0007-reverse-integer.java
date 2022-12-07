ff5d4cce8081fa37c3ca643f1fc0a118a6e902e3
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-12-08 02:42:14 +0530

    Time: 34 ms (5.04%), Space: 42.6 MB (5.68%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -2,17 +2,18 @@ class Solution {
     public int reverse(int x) {
         //since we have to deal with upto 2^32 means we can solve this within int. and for 2^64 we can use
         // if diving with 10 if not comes the same means garbage value collected and hence we can identify big no,
-        int res=0;
+        long res=0;
         while(x!=0)
         {
-            int temp= res*10 + x%10;
+            long temp= res*10 + x%10;
             System.out.println(temp+" "+res);
-            if(temp/10!=res)
-                return 0;
+            //if(temp/10!=res)
+                //return 0;
             res=temp;
             x=x/10;
         }
-        
-        return res;
+        //another way is to do like below
+        if(res>Integer.MAX_VALUE || res<Integer.MIN_VALUE)return 0;
+        return (int)res;
     }
 }
