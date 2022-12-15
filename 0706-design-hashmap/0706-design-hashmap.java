23531193bda8534511758aa45e7b2f4cb3b4c50e
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-12-16 00:36:44 +0530

    Time: 45 ms (59.27%), Space: 50.1 MB (83.66%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,23 +1,37 @@
 class MyHashMap {
 
-    HashMap<Integer,Integer> hm;
+    //HashMap<Integer,Integer> hm;
+    
+    int hash[];
     public MyHashMap() {
-        hm=new HashMap<>();
+        //hm=new HashMap<>();
+        
+        hash=new int[1000001];
+        Arrays.fill(hash,-1);//as 0 is also a key
     }
     
     public void put(int key, int value) {
         
-            hm.put(key,value);
+            //hm.put(key,value);
+        ///waht rubbish
+//this is giving me the value also tha t how much its passing
+        //System.out.println(key+" "+value);
+        
+        hash[key]=value;
     }
     
     public int get(int key) {
-        if(hm.containsKey(key))
-        return hm.get(key);
-            return -1;
+        //if(hm.containsKey(key))
+        //return hm.get(key);
+            //return -1;
+        
+        return hash[key];
     }
     
     public void remove(int key) {
-            hm.remove(key);
+            //hm.remove(key);
+        
+        hash[key]=-1; //reset
     }
 }
 
