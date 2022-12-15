25a8b5f3fefdd1122293a79f98b24b80b1c035bd
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-12-16 00:32:47 +0530

    Time: 13 ms (91.87%), Space: 46 MB (84.28%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,3 +1,20 @@
+/*
+algorithm: dymanic programming. M[i][j] be the LCS(Longest Common Subsequence) from text1[0 ... i - 1],
+and text2[0 ... j - 1], i and j stands for the length.
+smallest problem:
+    M[0][j] = 0     --> "" vs "..."
+        M[i][0] = 0     --> "..." vs ""
+        induction rule:
+            M[i][j] = 1 + M[i - 1][j - 1] if text1[i - 1] == text2[j - 1]
+                        = max(M[i - 1][j],      //skip current char in text1
+                            M[i][j - 1],      //skip current char in text2
+                            M[i - 1][j - 1]   //skip char in both text1 and text2
+                    );
+Complexity:
+Time: O(mn), m is length of text1, n is length of text2
+Space: O(mn) --> can be optimized to O(min(m, n)) since only accessing previous row
+*/
+
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int n1=text1.length(),n2=text2.length();
