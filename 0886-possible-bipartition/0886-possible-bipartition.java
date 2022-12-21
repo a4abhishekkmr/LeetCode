4e19ba1bee53a267efca6797b0b9970f31da8cc3
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-12-22 02:40:45 +0530

    Time: 11 ms (98.95%), Space: 49.6 MB (99.13%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,36 +1,40 @@
 class Solution {
 
-    private List<Integer>[] graph;
-    private int[] colors;
 
     public boolean possibleBipartition(int n, int[][] dislikes) {
 
-        //noinspection unchecked
-        graph = new List[n + 1];
+        int color[]=new int[n+1];
+        List<Integer>[] graph = new List[n + 1];
         for (int i = 1; i <= n; ++i) graph[i] = new ArrayList<>();
 
+        //array hai pura as a dislike
         for (int[] dislike : dislikes) {
             graph[dislike[0]].add(dislike[1]);
             graph[dislike[1]].add(dislike[0]);
         }
-
-        colors = new int[n + 1]; // 0=unvisited/no_color, 1=blue, -1=red
-        for (int i = 1; i <= n; ++i) {
-            if (colors[i] == 0 && canNotBiPartition(i, 1)) return false;
-        }
-
-        return true;
-    }
-
-    private boolean canNotBiPartition(int node, int color) {
-        colors[node] = color;
-        for (int neighbor : graph[node]) {
-            if (colors[neighbor] == 0) {
-                if (canNotBiPartition(neighbor, -color)) return true;
-            } else if (colors[neighbor] == color) {
-                return true;
+        //1=blue, -1=red
+        for(int i=1;i<=n;i++)
+        {
+            if(color[i]==0)
+            {
+                LinkedList<Integer> queue=new LinkedList<>();
+                queue.add(i);
+                color[i]=1;
+                while(queue.size()!=0)
+                {
+                    int top=queue.removeFirst();
+                    for(int neighbour:graph[top])
+                    {
+                        if(color[neighbour]==color[top]) return false;//check for current dislike element
+                        if(color[neighbour]==0)
+                        {//add new item
+                            color[neighbour]=-color[top];
+                            queue.add(neighbour);
+                        }
+                    }
+                }
             }
         }
-        return false;
+        return true;
     }
 }
