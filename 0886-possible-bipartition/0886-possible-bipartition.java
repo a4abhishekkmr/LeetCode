59f73889cf881873894c7432cedfbabd74e16d37
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-12-21 23:54:39 +0530

    Time: 18 ms (89.30%), Space: 73.5 MB (42.46%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,35 +1,36 @@
 class Solution {
-    public boolean possibleBipartition(int n, int[][] dislikes) {
-        if(dislikes.length == 0) return true;
-          HashMap<Integer,ArrayList<Integer>> map = new HashMap<>();
-          for(int i = 1 ; i <= n ; i++){
-              map.put(i,new ArrayList<Integer>());
-          }
-          for(int[] d : dislikes){
-              map.get(d[0]).add(d[1]);
-          }
-          int[] color = new int[n+1];
-          for(int i = 0 ; i < n+1 ; i++){
-              color[i] = -1;
-          }
-          Queue<Integer> que = new ArrayDeque<>();
 
-         for(int j = 1 ; j <= n ; j++){ 
-             que.offer(j);
-          while(!que.isEmpty()){
-              int v = Integer.valueOf(que.poll());
-              if(color[v] == -1) color[v] = v;
-            
-              for(int i = 0 ; i < map.get(v).size() ; i++){
-                  if(color[map.get(v).get(i)] == -1){
-                     color[map.get(v).get(i)] = 1 - color[v]; 
-                     que.offer(map.get(v).get(i));
-                  }
-                  else if(color[map.get(v).get(i)] == color[v]) return false;
-               }
-       
-          }
-         }
-          return true;
+    private List<Integer>[] graph;
+    private int[] colors;
+
+    public boolean possibleBipartition(int n, int[][] dislikes) {
+
+        //noinspection unchecked
+        graph = new List[n + 1];
+        for (int i = 1; i <= n; ++i) graph[i] = new ArrayList<>();
+
+        for (int[] dislike : dislikes) {
+            graph[dislike[0]].add(dislike[1]);
+            graph[dislike[1]].add(dislike[0]);
+        }
+
+        colors = new int[n + 1]; // 0=unvisited/no_color, 1=blue, -1=red
+        for (int i = 1; i <= n; ++i) {
+            if (colors[i] == 0 && canNotBiPartition(i, 1)) return false;
+        }
+
+        return true;
+    }
+
+    private boolean canNotBiPartition(int node, int color) {
+        colors[node] = color;
+        for (int neighbor : graph[node]) {
+            if (colors[neighbor] == 0) {
+                if (canNotBiPartition(neighbor, -color)) return true;
+            } else if (colors[neighbor] == color) {
+                return true;
+            }
+        }
+        return false;
     }
 }
