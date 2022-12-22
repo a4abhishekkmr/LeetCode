8cfa54f06b6ae7401f994213acf517d3d509bcbf
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2022-12-22 23:36:45 +0530

    Time: 94 ms (50.00%), Space: 66.1 MB (80.77%) - LeetHub

---
+++ 0834-sum-of-distances-in-tree/0834-sum-of-distances-in-tree.java
@@ -0,0 +1,45 @@
+class Solution {
+    
+    int[] ans,count;
+    List<Set<Integer>> graph;
+    int n;
+    public int[] sumOfDistancesInTree(int n, int[][] edges) {
+        //the return int will be of size n so that it will contain 0 to n-1 elements and next we habe
+       this.n=n;
+        graph=new ArrayList<Set<Integer>>();
+        ans=new int[n];
+        count=new int[n];
+        Arrays.fill(count,1);
+        
+        for(int i=0;i<n;++i)
+            graph.add(new HashSet<Integer>());
+        
+        for(int []edge:edges)
+        {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        
+        dfs(0,-1);
+        dfs2(0,-1);
+        return ans;
+    }
+    public void dfs(int node,int parent)
+    {
+        for(int child:graph.get(node))
+            if(child!=parent)
+               { dfs(child,node);
+            count[node]+=count[child];
+            ans[node]+=ans[child]+count[child];
+        }
+    }
+    public void dfs2(int node,int parent)
+    {
+        for(int child:graph.get(node))
+            if(child!=parent)
+            {
+                ans[child]=ans[node]-count[child]+n-count[child];
+                dfs2(child,node);
+            }
+    }
+}
