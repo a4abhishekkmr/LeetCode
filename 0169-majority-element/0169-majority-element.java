bae0e8f9f46cda3f843822da0b57afd38680eb01
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2023-01-15 22:42:33 +0530

    Time: 2 ms (84.13%), Space: 46.2 MB (67.44%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -6,3 +6,34 @@ class Solution {
         return (nums[nums.length/2]);
     }
 }
+
+/*
+public int majorityElement3(int[] nums) {
+    int count=0, ret = 0;
+    for (int num: nums) {
+        if (count==0)
+            ret = num;
+        if (num!=ret)
+            count--;
+        else
+            count++;
+    }
+    return ret;
+}
+
+// Bit manipulation 
+public int majorityElement(int[] nums) {
+    int[] bit = new int[32];
+    for (int num: nums)
+        for (int i=0; i<32; i++) 
+            if ((num>>(31-i) & 1) == 1)
+                bit[i]++;
+    int ret=0;
+    for (int i=0; i<32; i++) {
+        bit[i]=bit[i]>nums.length/2?1:0;
+        ret += bit[i]*(1<<(31-i));
+    }
+    return ret;
+}
+
+*/
