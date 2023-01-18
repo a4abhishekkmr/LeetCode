84db0410408df9062fe12e61a4f1c691acab6db2
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2023-01-18 10:21:50 +0530

    Time: 5 ms (100.00%), Space: 48.5 MB (64.79%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -19,3 +19,33 @@ class Solution {
         return false;
     }  
 }
+
+//Another approach
+/*
+
+//O(N log N)
+class Solution {
+    
+    
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n =matrix.length;
+        int m = matrix[0].length;
+        
+        for(int i = 0 ; i< n ; i++)
+        {
+            int left = 0;
+            int right = m-1;
+            
+            while(left<=right)
+            {
+                int mid  = left + (right-left)/2;
+                if(matrix[i][mid] == target)return true;
+                else if(matrix[i][mid] < target)left = mid+1;
+                else right = mid - 1;
+            }
+        }
+      
+        return false;
+    }
+    
+} */
