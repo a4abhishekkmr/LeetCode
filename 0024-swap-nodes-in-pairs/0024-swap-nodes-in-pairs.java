b820a126bbe56d4e7509c828eff2b443aac535c8
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2023-01-30 16:22:15 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (96.86%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -10,20 +10,23 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        
+        //checking for length 0 or 1
         if(head==null || head.next==null) return head;
-        
+        //taking two pointers in 1st and second position
+		
         ListNode slow=head;
         ListNode fast=head.next;
         
         while(fast!=null && fast.next!=null )
-        {
+        {//swap
             int temp=slow.val;
             slow.val=fast.val;
             fast.val=temp;
+			//move 2 steps ahead both pointers
             fast=fast.next.next;
             slow=slow.next.next;
         }
+		//for even i need to swap again
         if(fast!=null)
         {
             int temp=slow.val;
