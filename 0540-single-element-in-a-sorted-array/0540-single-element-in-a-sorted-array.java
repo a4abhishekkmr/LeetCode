1b5f80ac9aff2582a02edddca64a054cea038983
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2023-02-21 22:47:21 +0530

    Time: 0 ms (100.00%), Space: 60.2 MB (10.48%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,10 +1,31 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         //first lets go wit O(n) approach
-        for(int i=0;i<nums.length-1;i+=2)
+        // for(int i=0;i<nums.length-1;i+=2)
+        // {
+        //     if(nums[i]!=nums[i+1]) return nums[i];
+        // }
+        // return nums[nums.length-1];
+        
+        // its giving me 30% fater among all the solutions lets go with more faster solution then
+        //     so i am looking for Binary search solution as we have sorted array and to get benefit of that
+        //         we can do one thing is we can check for mid and compare it
+                    
+        int i=0,j=nums.length-1;
+        
+        //int mid=(i+j)/2;
+        
+        while(i<j)
         {
-            if(nums[i]!=nums[i+1]) return nums[i];
+            int mid=i+(j-i)/2;
+            
+            //if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1] ) return nums[mid];
+            if(mid%2==0) mid--;
+            if(nums[mid] == nums[mid-1])
+                i = mid+1;
+            else
+               j = mid-1;
         }
-        return nums[nums.length-1];
+        return nums[i];
      }
 }
