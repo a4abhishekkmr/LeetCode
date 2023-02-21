a1ced5c56f83e879b6bcc44f6af6adbbbe5f0dbe
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2023-02-21 22:52:56 +0530

    Time: 1 ms (30.03%), Space: 50.8 MB (10.80%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -26,3 +26,53 @@ class Solution {
         return newNode;
     }
 }
+// //TreeNode left= mergeTrees(root1.left, root2.left);//recursing down the left subtree and knowing about the left child 
+//         TreeNode right= mergeTrees(root1.right, root2.right);//recursing down the right subtree and knowing about the right child 
+      
+//         //ROOT
+//         //creating the node by the total information received from left and right child 
+//         TreeNode node= new TreeNode(root1.val+root2.val, left, right);
+      
+//         return node;//returning the node in order to maintain the backward modified link at each instant//Telling to the parent that I am present  
+//     }
+
+/*class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) 
+    {
+        Stack <TreeNode> s = new Stack <TreeNode>();
+        s.push(t1);
+        s.push(t2);
+        if (t1 == null)
+            return t2;
+ 
+        while(!s.isEmpty())
+        {
+            TreeNode two = s.pop();
+            TreeNode one = s.pop();
+            
+            if (one != null && two != null)
+            {
+                one.val += two.val;
+                if (one.left == null){
+                    one.left = two.left;
+                }
+                else
+                {
+                    s.push(one.left);
+                    s.push(two.left);
+                }
+                      
+                if (one.right == null){
+                    one.right = two.right;
+                }
+                else
+                {
+                    s.push(one.right);
+                    s.push(two.right);
+                }  
+            }     
+        }
+        return t1;  
+    }
+}
+*/
