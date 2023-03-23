86761b4376b499f9db6dab17df32dbb48ad640f0
Abhishek kumar <61092638+a4abhishekkmr@users.noreply.github.com>
2023-03-24 03:12:49 +0530

    Time: 22 ms (35.39%), Space: 66.6 MB (21.43%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -24,6 +24,17 @@ class Solution {
 All of the computers are connected to some other computers. But they don't form one connected network. You need to find the number of such DN (disconnected networks aka disjoint sets), in this case DN=2 (1,2,3 and 4,5).
 And the answer is DN-1. */
         
+        
+        /*Intuition
+we will use dfs to solve this, padhte rho lg jaayega pta
+
+Approach
+first traverse over the array. and create a hashset array where each index ke paas padha h ki usse kitne or computer connected hai
+for example at index 0 we will have ki 0 se kon kon connected hai
+ab hum dfs lgaa rhe, maanlo shuru kr rhe 0 se , 0 will visit 1 and 2
+3 reh jaayega, uske liye ans++ hoga,
+basic logic ye h ki agar, hum vo vo nikaal le ki jitne computer h jo connected nhi h, mtlb see example below
+. ab loop return kredegi kitne components h jo alag alag hai, agar aise 4 h to hume 3 wires chaye, so ans -1, or start ans from -1*/
         HashSet<Integer>[] edges = new HashSet[n];
         for(int i=0;i<n;i++) {
             edges[i] = new HashSet<>();
